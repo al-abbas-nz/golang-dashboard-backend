f6ecc96653d2592c72140b6a1a7f805b61c1954c
al-abbas-nz <al.mus930@gmail.com>
2021-03-04 19:47:15 +1300

    authenticated user

--- controllers/authController.go
+++ controllers/authController.go
@@ -91,3 +91,28 @@ func Login(c *fiber.Ctx) error {
 		"message": "Success.",
 	})
 }
+
+type Claims struct {
+	jwt.StandardClaims
+}
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+	token, err := jwt.ParseWithClaims(cookie, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+
+	if err != nil || !token.Valid {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "Unauthenticated",
+		})
+	}
+	claims := token.Claims.(*Claims)
+
+	var user models.User
+
+	database.DB.Where("id =?", claims.Issuer).First(&user)
+
+	return c.JSON(user)
+}
